a7e52fc68dd482a00ec11c840985a9b4adb4d8b4
Rizzags <agustinriza20@yahoo.com>
2019-04-20 18:43:25 +0700

    Insert Data registration to database, form validation;

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -9,16 +9,23 @@
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                             </div>
-                            <form class="user">
+                            <form class="user" method="POST" action="<?php echo base_url('auth/registration') ?>">
                                 <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full Name">
+                                    <input type="text" class="form-control form-control-user" id="name" name="name" 
+                                    value="<?php echo set_value('name') ?>" 
+                                    placeholder="Full Name">
+                                   <?= form_error("name"," <smal class='text-danger pl-3'>","</smal>") ?>
                                 </div>
                                 <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address">
+                                    <input type="text" class="form-control form-control-user" id="email" name="email"  value="<?php echo set_value('email') ?>"placeholder="Email Address">
+                                     
+                                    <?= form_error("email","<smal class='text-danger pl-3'>","</smal>") ?>
+                                    
                                 </div>
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
                                         <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password">
+                                        <?= form_error("password1"," <smal class='text-danger pl-3'>","</smal>") ?>
                                     </div>
                                     <div class="col-sm-6">
                                         <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Repeat Password">
